0b25a49d00e56fe3286704b7a3fe190ee1c7d4f7
alsdud <alsdud2673@naver.com>
2023-11-06 17:11:52 +0900

    #2 - Querydsl dependency, gitignore 추가 및 설정
    Querydsl 사용할 수 있는 환경을 구성
    자동 생성 클래스는 프로젝트에 포함되면 안되기 때문에` .gitignore`로 제외
    
    gradle spring 버전 강의 자료와 동일하게 수정.

--- fastcampus-board/src/main/java/com/fastcampus/board/domain/ArticleComment.java
+++ fastcampus-board/src/main/java/com/fastcampus/board/domain/ArticleComment.java
@@ -1,10 +1,10 @@
 package com.fastcampus.board.domain;
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
--- fastcampus-board/src/main/java/com/fastcampus/board/repository/ArticleRepository.java
+++ fastcampus-board/src/main/java/com/fastcampus/board/repository/ArticleRepository.java
@@ -1,9 +1,31 @@
 package com.fastcampus.board.repository;
 
 import com.fastcampus.board.domain.Article;
+import com.fastcampus.board.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.sql.DatabaseMetaData;
+
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+// 기본적으로 엔티티 안에 있는 기본 검색기능을 추가해준다.
+        QuerydslBinderCustomizer<QArticle> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content , root.hashtag, root.createdAt , root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::eq);
+    }
 }
