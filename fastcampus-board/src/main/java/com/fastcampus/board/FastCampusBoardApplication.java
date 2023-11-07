ea268497e5ba8c1362239b71787faf8ad512df98
alsdud <alsdud2673@naver.com>
2023-11-07 14:43:04 +0900

    #10 - tymeleaf 3 decoupled logic 설정
    spring boot configuration processor 디펜던시는 사용자정의 application property를 좀 더  IDE에서 편리하게 연동하여 사용하기 위해 디펜던시 추가
    
    java code 로  thymeleaf 3 decoupled 로직을 사용자 application property 로 제어할 수 있게 설정 추가

--- fastcampus-board/src/main/java/com/fastcampus/board/FastCampusBoardApplication.java
+++ fastcampus-board/src/main/java/com/fastcampus/board/FastCampusBoardApplication.java
@@ -2,8 +2,10 @@ package com.fastcampus.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class FastCampusBoardApplication {
 
 	public static void main(String[] args) {
